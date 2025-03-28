85c3f409c60c724b08fae27adaef58c9ffd437aa
Adam Varsányi <xvarsa01@vutbr.cz>
2025-03-28 18:32:20 +0100

    feat: Added global facade and model for searching across everything

---
+++ Stofipy.BL/Facades/GlobalFacade.cs
@@ -0,0 +1,54 @@
+﻿using Stofipy.BL.Mappers;
+using Stofipy.BL.Models;
+using Stofipy.DAL.Entities;
+using Stofipy.DAL.Repositories;
+
+namespace Stofipy.BL.Facades;
+
+public class GlobalFacade
+{
+    private readonly FileRepository _fileRepository;
+    private readonly AuthorRepository _authorRepository;
+    private readonly AlbumRepository _albumRepository;
+    private readonly PlaylistRepository _playlistRepository;
+
+    private readonly FileModelMapper _fileModelMapper;
+    private readonly AuthorModelMapper _authorModelMapper;
+    private readonly AlbumModelMapper _albumModelMapper;
+    private readonly PlaylistModelMapper _playlistModelMapper;
+
+    public GlobalFacade(FileRepository fileRepository, AuthorRepository authorRepository, AlbumRepository albumRepository, PlaylistRepository playlistRepository, FileModelMapper fileModelMapper, AuthorModelMapper authorModelMapper, AlbumModelMapper albumModelMapper, PlaylistModelMapper playlistModelMapper)
+    {
+        _fileRepository = fileRepository;
+        _authorRepository = authorRepository;
+        _albumRepository = albumRepository;
+        _playlistRepository = playlistRepository;
+        _fileModelMapper = fileModelMapper;
+        _authorModelMapper = authorModelMapper;
+        _albumModelMapper = albumModelMapper;
+        _playlistModelMapper = playlistModelMapper;
+    }
+
+    public async Task<GlobalSearchModel> SearchGloballyAsync(Guid playlistId,string searchTerm)
+    {
+        List<FileEntity> fileEntities = await _fileRepository.SearchInFilesAsync(searchTerm);
+        List<FileListModel> files = _fileModelMapper.MapToListModel(fileEntities);
+        
+        List<AuthorEntity> authorEntities = await _authorRepository.SearchInAuthorsAsync(searchTerm);
+        List<AuthorListModel> authors = _authorModelMapper.MapToListModel(authorEntities);
+        
+        List<AlbumEntity> albumEntities = await _albumRepository.SearchInalbumsAsync(searchTerm);
+        List<AlbumListModel> albums = _albumModelMapper.MapToListModel(albumEntities);
+        
+        List<PlaylistEntity> playlistEntities = await _playlistRepository.SearchInPlaylistsAsync(searchTerm);
+        List<PlaylistListModel> playlists = _playlistModelMapper.MapToListModel(playlistEntities);
+
+        return new GlobalSearchModel()
+        {
+            Files = files,
+            Authors = authors,
+            Albums = albums,
+            Playlists = playlists,
+        };
+    }
+}
---
+++ Stofipy.BL/Models/GlobalSearchModel.cs
@@ -0,0 +1,12 @@
+﻿using Stofipy.BL.Models.InterfaceBase;
+using Stofipy.DAL.Entities;
+
+namespace Stofipy.BL.Models;
+
+public record GlobalSearchModel()
+{
+    public List<FileListModel> Files { get; set; } = [];
+    public List<AuthorListModel> Authors { get; set; } = [];
+    public List<AlbumListModel> Albums { get; set; } = [];
+    public List<PlaylistListModel> Playlists { get; set; } = [];
+}
