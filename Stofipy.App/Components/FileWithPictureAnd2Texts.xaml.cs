6801391b977b58c4b9a4378644df4ccdca0f4335
xvarsa01 <xvarsa01@vutbr.cz>
2025-05-21 12:21:37 +0200

    feat: Created separate component FileWithPictureAnd2Texts.xaml

---
+++ Stofipy.App/Components/FileWithPictureAnd2Texts.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace Stofipy.App.Components;
+
+public partial class FileWithPictureAnd2Texts : ContentView
+{
+    public FileWithPictureAnd2Texts()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+    
+    public static readonly BindableProperty PictureProperty =
+        BindableProperty.Create(nameof(Picture), typeof(ImageSource), typeof(FileWithPictureAnd2Texts));
+
+    public ImageSource Picture
+    {
+        get => (ImageSource)GetValue(PictureProperty);
+        set => SetValue(PictureProperty, value);
+    }
+
+    public static readonly BindableProperty TopTextProperty =
+        BindableProperty.Create(nameof(TopText), typeof(string), typeof(FileWithPictureAnd2Texts));
+
+    public string TopText
+    {
+        get => (string)GetValue(TopTextProperty);
+        set => SetValue(TopTextProperty, value);
+    }
+
+    public static readonly BindableProperty BottomTextProperty =
+        BindableProperty.Create(nameof(BottomText), typeof(string), typeof(FileWithPictureAnd2Texts));
+
+    public string BottomText
+    {
+        get => (string)GetValue(BottomTextProperty);
+        set => SetValue(BottomTextProperty, value);
+    }
+}
