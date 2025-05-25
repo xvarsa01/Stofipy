2fb03e0f38120f983f56c547881c9da2e0430e18
xvarsa01 <xvarsa01@vutbr.cz>
2025-05-25 20:04:26 +0200

    fix: Removed unused ViewModels in Navigation service that made circular dependency.

--- Stofipy.App/Services/NavigationService.cs
+++ Stofipy.App/Services/NavigationService.cs
@@ -6,19 +6,6 @@ namespace Stofipy.App.Services;
 
 public class NavigationService : INavigationService
 {
-    private readonly ListOfPlaylistsVM _listOfPlaylistsVM;
-    private readonly PlaylistDetailVM _playlistDetailVM;
-    
-    private readonly AuthorDetailVM _authorDetailVM;
-    private readonly FilesInQueueVM _filesInQueueVM;
-
-    public NavigationService(ListOfPlaylistsVM listOfPlaylistsVM, FilesInQueueVM filesInQueueVM, PlaylistDetailVM playlistDetailVM, AuthorDetailVM authorDetailVM)
-    {
-        _listOfPlaylistsVM = listOfPlaylistsVM;
-        _playlistDetailVM = playlistDetailVM;
-        _authorDetailVM = authorDetailVM;
-        _filesInQueueVM = filesInQueueVM;
-    }
 
     public event Func<NavigationRequest, Task>? NavigationRequested;
 
