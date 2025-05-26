cd8811481222f5f3461b8160c28726914596a814
xvarsa01 <xvarsa01@vutbr.cz>
2025-05-26 23:20:49 +0200

    feat: Created empty DotVVM project

---
+++ Stofipy.App2/DotvvmStartup.cs
@@ -0,0 +1,60 @@
+﻿using DotVVM.Framework.Configuration;
+using DotVVM.Framework.ResourceManagement;
+using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stofipy.App2
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureResources(config, applicationPath);
+
+            // https://www.dotvvm.com/docs/4.0/pages/concepts/configuration/explicit-assembly-loading
+            config.ExperimentalFeatures.ExplicitAssemblyLoading.Enable();
+
+            // Use this for command heavy applications
+            // - DotVVM will store the viewmodels on the server, and client will only have to send back diffs
+            // https://www.dotvvm.com/docs/4.0/pages/concepts/viewmodels/server-side-viewmodel-cache
+            // config.ExperimentalFeatures.ServerSideViewModelCache.EnableForAllRoutes();
+
+            // Use this if you are deploying to containers or slots
+            //  - DotVVM will precompile all views before it appears as ready
+            // https://www.dotvvm.com/docs/4.0/pages/concepts/configuration/view-compilation-modes
+            // config.Markup.ViewCompilation.Mode = ViewCompilationMode.DuringApplicationStart;
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("Default", "", "Views/Default.dothtml");
+
+            // Uncomment the following line to auto-register all dothtml files in the Views folder
+            // config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config, viewsFolder: "Views"));   
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+            // register code-only controls and markup controls
+        }
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+            // register custom resources and adjust paths to the built-in resources
+            config.Resources.Register("Styles", new StylesheetResource()
+            {
+                Location = new UrlResourceLocation("~/Resources/style.css")
+            });
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("temp");
+            options.AddHotReload();
+        }
+    }
+}
---
+++ Stofipy.App2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DotVVM.Framework.Hosting;
+
+using Stofipy.App2;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddAuthentication();
+builder.Services.AddDotVVM<DotvvmStartup>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseDotVVM<DotvvmStartup>();
+app.MapDotvvmHotReload();
+
+app.Run();
