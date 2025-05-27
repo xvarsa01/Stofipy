5cd30d86c5e7c2e116c926572f1d0a7b481bd782
xvarsa01 <xvarsa01@stud.fit.vutbr.cz>
2025-05-27 18:42:00 +0200

    feat: PlaylistDetail view

---
+++ Stofipy.App2/ViewModels/PlaylistDetailViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.ViewModel;
+using Stofipy.BL.Facades;
+using Stofipy.BL.Facades.Interfaces;
+using Stofipy.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stofipy.App2.ViewModels
+{
+    public class PlaylistDetailViewModel(
+        IPlaylistFacade playlistFacade,
+        IFilesInPlaylistFacade filesInPlaylistFacade,
+        IFilesInQueueFacade filesInQueueFacade) : MasterPageViewModel
+    {
+        [FromRoute("id")]
+        private Guid Id { get; set; }
+        public PlaylistDetailModel Playlist { get; set; } = null!;
+        public List<FilesInPlaylistModel> Files { get; set; }
+
+        public override async Task PreRender()
+        {
+            if (Playlist == null)
+            {
+                await LoadDataAsync();
+            }
+            await base.PreRender();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            Playlist = await playlistFacade.GetByIdAsync(Id) ?? PlaylistDetailModel.Empty;
+            Files = await filesInPlaylistFacade.GetAllAsync(Id, 1, 10);
+        }
+
+
+        public async Task PlayPlaylistAsync()
+        {
+            await filesInQueueFacade.AddPlaylistToQueue(Playlist.Id, false);
+        }
+
+        public void Shuffle()
+        {
+
+        }
+
+        public void Download()
+        {
+
+        }
+
+    }
+}
+
