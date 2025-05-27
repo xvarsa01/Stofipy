eb144a2626f12e401cbaf3bc2de896182ab0ddbf
xvarsa01 <xvarsa01@vutbr.cz>
2025-05-27 03:02:22 +0200

    feat: Author detail view in DotVVM

---
+++ Stofipy.App2/ViewModels/AuthorDetailViewModel.cs
@@ -0,0 +1,93 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.ViewModel;
+using Stofipy.BL.Facades.Interfaces;
+using Stofipy.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stofipy.App2.ViewModels
+{
+    public class AuthorDetailViewModel(
+        IAuthorFacade facade,
+        IFileFacade fileFacade) : MasterPageViewModel()
+    {
+        private Guid Id { get; set; } = Guid.Parse("63E304C0-F051-436D-A466-3048C1C0D31F");
+        public AuthorDetailModel Author { get; set; }
+
+        private FileListModel SelectedFile { get; set; }
+        public List<FileListModel> PopularFilesCurrentlyShowed { get; set; } = new();
+        private List<FileListModel> PopularFiles5 { get; set; } = new();
+        private List<FileListModel> PopularFiles10 { get; set; } = new();
+
+
+        public bool MorePopularFilesShowed { get; set; } = false;
+        public string SeeMoreText { get; set; } = "Show more";
+        private bool _following = false;
+
+
+        public override async Task Init()
+        {
+            if (Author == null)
+            {
+                await LoadByIdAsync(Id);
+            }
+            
+            await base.Init();
+        }
+
+        public async Task LoadByIdAsync(Guid id)
+        {
+            Id = id;
+            await LoadDataAsync();
+        }
+
+        private async Task LoadDataAsync()
+        {
+            Author = await facade.GetByIdAsync(Id);
+            PopularFiles5 = await fileFacade.GetMostPopularFiles(Id, 1, 5);
+            PopularFilesCurrentlyShowed = PopularFiles5;
+        }
+
+        public async Task ShowMorePopularFiles()
+        {
+            if (!MorePopularFilesShowed)
+            {
+                if (PopularFiles10.Count == 0)
+                {
+                    var newFiles = await fileFacade.GetMostPopularFiles(Id, 2, 5);
+                    foreach (var file in newFiles)
+                    {
+                        PopularFilesCurrentlyShowed.Add(file);
+                    }
+                }
+                else
+                {
+                    PopularFilesCurrentlyShowed = PopularFiles10;
+                }
+
+                SeeMoreText = "Show less";
+                MorePopularFilesShowed = true;
+            }
+            else
+            {
+                PopularFiles10 = PopularFilesCurrentlyShowed;
+                PopularFilesCurrentlyShowed = PopularFiles5;
+
+                SeeMoreText = "Show more";
+                MorePopularFilesShowed = false;
+            }
+        }
+
+
+        public void PlayArtist()
+        {
+            Console.WriteLine($"Playing artist");
+        }
+
+    }
+}
+
