92447b13d9613dd5c76bc9562f860ebee5076718
xvarsa01 <xvarsa01@vutbr.cz>
2025-06-16 23:21:06 +0200

    refactor: Separated highlighted drop area into component

---
+++ Stofipy.App/Components/QueueHighlitedDropArea.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stofipy.App.Components;
+
+public partial class QueueHighlightedDropArea : ContentView
+{
+    public QueueHighlightedDropArea()
+    {
+        InitializeComponent();
+    }
+    
+    public static readonly BindableProperty HighlightedProperty =
+        BindableProperty.Create(nameof(Highlighted), typeof(bool), typeof(QueueHighlightedDropArea));
+
+    public bool Highlighted
+    {
+        get => (bool)GetValue(HighlightedProperty);
+        set => SetValue(HighlightedProperty, value);
+    }
+}
