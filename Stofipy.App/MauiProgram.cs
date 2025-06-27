1fdf31384a0c309eeb2a383198acdb3ebbb966f4
Adam Varsányi <adam.varsanyi@riganti.cz>
2025-06-27 16:11:04 +0200

    feat: Right section UI improved

--- Stofipy.App/MauiProgram.cs
+++ Stofipy.App/MauiProgram.cs
@@ -25,6 +25,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("FuturaStdExtraBold.otf", "FuturaBold");
+                fonts.AddFont("CircularSpotifyText-Bold.otf", "Circular");
             });
         
         ConfigureAppSettings(builder);
